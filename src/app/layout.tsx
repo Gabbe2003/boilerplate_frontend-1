04cc5b6f5c53b3732f154f595dfc348da8c417ef
Gabbe2003 <gabriell.touma@gmail.com>
2025-12-06 23:04:44 +0100

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import Script from "next/script";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 import Footer from "@/components/Footer/Footer";
@@ -7,27 +8,27 @@ import "../styles/globals.css";
 import AdSlot from "@/components/Ads/adsenseBanner";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  const adsenseClient = process.env.ADSENSE_CLIENT;
+  const adsenseClient = process.env.NEXT_PUBLIC_ADSENSE_CLIENT;
 
   return (
     <html lang="sv">
       <body>
-    <>
-      {adsenseClient ? (
-        <script
-          async
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsenseClient}`}
-          crossOrigin="anonymous"
-        />
-      ) : null}
-    </>     
-    
+        {adsenseClient ? (
+          <Script
+            id="adsense-loader"
+            async
+            strategy="afterInteractive"
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsenseClient}`}
+            crossOrigin="anonymous"
+          />
+        ) : null}
+
         <div className="w-full flex flex-col items-center">
           <HeaderWrapper />
           <div className="w-full max-w-[1100px] px-4 my-4">
             {/* <AdSenseBanner /> */}
           </div>
-        <AdSlot adSlot="12"/>
+          {adsenseClient ? <AdSlot adSlot="12" /> : null}
 
           {children}
           <Footer />

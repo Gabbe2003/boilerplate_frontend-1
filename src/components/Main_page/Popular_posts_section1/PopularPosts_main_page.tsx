3654a7f105b63a776c799461c394224ed7774494
joker12312-12 <jokerfamily08@gmail.com>
2025-12-10 13:47:20 +0100

    Fixed style

--- src/components/Main_page/Popular_posts_section1/PopularPosts_main_page.tsx
+++ src/components/Main_page/Popular_posts_section1/PopularPosts_main_page.tsx
@@ -20,11 +20,11 @@ export default async function Home() {
     getAllPosts(6),
     getPostsByPeriod("week"),
   ]) as [Post[], Post[]];
-  
-  const left_col: Post[] = posts.slice(0,5);
+
+  const left_col: Post[] = posts.slice(0, 5);
   const middle_big: Post = posts[3];
   const middle_rest: Post[] = posts.slice(4);
-  const post_right = todays_right?.slice(0,6) ?? [];
+  const post_right = todays_right?.slice(0, 6) ?? [];
 
   return (
     <main className="section1-border-theme w-full flex justify-center font-serif pt-5  pb-(--section-spacing)">
@@ -32,9 +32,6 @@ export default async function Home() {
 
         {/* LEFT COLUMN */}
         <aside className="md:col-span-3 flex flex-col gap-4 order-3 md:order-1">
-          <h2 className="text-lg font-semibold mb-2 text-center sm:text-left">
-              Senaste nyheter
-            </h2>
           {left_col.map((post: Post) => (
             <Link
               key={post.id}
@@ -68,49 +65,49 @@ export default async function Home() {
           </div>
 
         </aside>
-        
 
-{/* MIDDLE COLUMN */}
-<section className="md:col-span-6 flex flex-col gap-6 order-1 md:order-2 p-0 sm:p-2 border-0 md:border md:border-[#e4d8ce]">
-  <h2 className="text-center items-center text-2xl">
-    Dagliga nyheter inom finans, aktier och börsen
-  </h2>
 
-  {/* Large Article */}
-  {middle_big && (
-    <Link
-      href={`/${middle_big.slug}`}
-      className="group block overflow-hidden p-0 sm:p-2"
-    >
-      <div className="relative w-full aspect-[4/3] overflow-hidden flex items-center justify-center">
-        <Image
-          src={middle_big.featuredImage?.node?.sourceUrl || "/favicon.ico"}
-          alt={middle_big.featuredImage?.node?.altText || middle_big.title!}
-          fill
-          className="object-cover"
-          priority
-          sizes="(max-width: 768px) 100vw, 700px"
-        />
-      </div>
+        {/* MIDDLE COLUMN */}
+        <section className="md:col-span-6 flex flex-col gap-6 order-1 md:order-2 p-0 sm:p-2 border-0 md:border md:border-[#e4d8ce]">
+          <h2 className="text-center items-center text-2xl">
+            Dagliga nyheter inom finans, aktier och börsen
+          </h2>
 
-      <div className="pt-6">
-        <h2 className="text-lg sm:text-xl font-bold leading-snug group-hover:underline group-hover:decoration-2 group-hover:decoration-offset-2">
-          {middle_big.title}
-        </h2>
+          {/* Large Article */}
+          {middle_big && (
+            <Link
+              href={`/${middle_big.slug}`}
+              className="group block overflow-hidden p-0 sm:p-2"
+            >
+              <div className="relative w-full aspect-[4/3] overflow-hidden flex items-center justify-center">
+                <Image
+                  src={middle_big.featuredImage?.node?.sourceUrl || "/favicon.ico"}
+                  alt={middle_big.featuredImage?.node?.altText || middle_big.title!}
+                  fill
+                  className="object-cover"
+                  priority
+                  sizes="(max-width: 768px) 100vw, 700px"
+                />
+              </div>
 
-        <p className="text-sm mt-3 line-clamp-3">
-          {limitExcerpt(middle_big.excerpt, 30)}
-        </p>
+              <div className="pt-6">
+                <h2 className="text-lg sm:text-xl font-bold leading-snug group-hover:underline group-hover:decoration-2 group-hover:decoration-offset-2">
+                  {middle_big.title}
+                </h2>
 
-        <div className="flex justify-between mt-3 text-xs text-[#6f6a63]">
-          <span className="text-red-500">
-            {capitalizeFirstLetter(middle_big.author?.node?.name) ?? ""}
-          </span>
-          <span>{formatDateStockholm(middle_big.date!)}</span>
-        </div>
-      </div>
-    </Link>
-  )}
+                <p className="text-sm mt-3 line-clamp-3">
+                  {limitExcerpt(middle_big.excerpt, 30)}
+                </p>
+
+                <div className="flex justify-between mt-3 text-xs text-[#6f6a63]">
+                  <span className="text-red-500">
+                    {capitalizeFirstLetter(middle_big.author?.node?.name) ?? ""}
+                  </span>
+                  <span>{formatDateStockholm(middle_big.date!)}</span>
+                </div>
+              </div>
+            </Link>
+          )}
 
 
           {/* Middle Rest */}
@@ -132,7 +129,7 @@ export default async function Home() {
                 <div
                   className="
                     relative 
-                    w-full lg:w-40 
+                    w-full lg:w-50 
                     aspect-4/3
                     lg:aspect-video
                     overflow-hidden 
@@ -173,60 +170,71 @@ export default async function Home() {
 
         {/* RIGHT COLUMN */}
         <aside className="md:col-span-3 flex flex-col gap-6 order-2 md:order-3">
-
           <div>
             <h2 className="text-lg font-semibold mb-2 text-center sm:text-left">
               Populära nyheter
             </h2>
 
-            <div className="flex flex-col border-b border-[#e4d8ce]">
-              {post_right.map((post: any) => (
-                post && (
-                  <Link
-                    key={post.id}
-                    href={`/${post.slug}`}
-                    className="
-                      group 
-                      flex flex-col sm:flex-row 
-                      gap-3 
-                      py-4 
-                      border-b
-                    "
-                  >
-                    <div
+            <div className="flex flex-col ">
+              {post_right.map((post: any, index: number) => (
+                <div key={post.id}>
+
+                  {/* Your normal post card */}
+                  {post && (
+                    <Link
+                      href={`/${post.slug}`}
                       className="
-                        relative 
-                        w-full sm:w-28 
-                        aspect-[4/3] 
-                        overflow-hidden 
-                        bg-[#f7f1ea]
+                        group 
+                        flex flex-col [@media(min-width:1271px)]:flex-row
+                        gap-3 
+                        py-4 
+                        border-b
                       "
                     >
-                      <Image
-                        src={post.featuredImage?.node?.sourceUrl || "/favicon.icon"}
-                        alt={post.featuredImage?.node?.altText || post.title!}
-                        fill
-                        className="object-cover transition-transform duration-200 group-hover:scale-[1.13]"
-                        sizes="(max-width: 640px) 100vw, 112px"
-                      />
-                    </div>
-
-                    <div className="flex-1 flex flex-col min-w-0">
-                      <h3 className="font-semibold text-sm leading-snug mb-1 break-words group-hover:underline group-hover:decoration-2 group-hover:decoration-offset-2">
-                        {post.title}
-                      </h3>
-
-                      <div className="flex justify-between items-center text-xs text-[#6f6a63] mt-1">
-                        <span className="text-red-500">
-                          {capitalizeFirstLetter(post.author?.node?.name) ?? ""}
-                        </span>
-                        <span>{formatDateStockholm(post.date!)}</span>
+                      <div
+                        className="
+                          relative 
+                          w-full [@media(min-width:1271px)]:w-28 
+                          aspect-[5/3]
+                          overflow-hidden 
+                          bg-[#f7f1ea]
+                        "
+                      >
+                        <Image
+                          src={post.featuredImage?.node?.sourceUrl || '/favicon.icon'}
+                          alt={post.featuredImage?.node?.altText || post.title!}
+                          fill
+                          className="object-cover transition-transform duration-200 group-hover:scale-[1.13]"
+                          sizes="100vw"
+                        />
                       </div>
+
+                      <div className="flex-1 flex flex-col min-w-0">
+                        <h3 className="font-semibold text-sm leading-snug mb-1 break-words group-hover:underline group-hover:decoration-2 group-hover:decoration-offset-2">
+                          {post.title}
+                        </h3>
+
+                        <div className="flex justify-between items-center text-xs text-[#6f6a63] mt-1">
+                          <span className="text-red-500">
+                            {capitalizeFirstLetter(post.author?.node?.name) ?? ''}
+                          </span>
+                          <span>{formatDateStockholm(post.date!)}</span>
+                        </div>
+                      </div>
+                    </Link>
+                  )}
+
+                  {/* Inject ReadPeak BETWEEN index 0–4 (e.g., after index 4) */}
+                  {index === 2 && (
+                    <div className="my-4 ">
+                      <ReadPeak  />
                     </div>
-                  </Link>
-                )
+                  )}
+
+                </div>
               ))}
             </div>
+
           </div>
 
           {/* ReadPeak pinned bottom */}
